1a7303728fb303d5895d0f08002d11ce232b2507
Leonardo Seibt <leonardoseibt@gmail.com>
2019-06-25 14:50:50 -0300

    Incluida a mensagem Everything is good

--- ConsoleAppTeste/Program.cs
+++ ConsoleAppTeste/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleAppTeste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good morning!");
+            Console.WriteLine("Everything is good!");
         }
     }
 }
