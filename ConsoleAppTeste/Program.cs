d1487e3041623788d61beea5175f2c760e81e318
Leonardo Seibt <leonardoseibt@gmail.com>
2019-06-25 11:57:25 -0300

    Projeto iniciado, mostrando a mensagem Hello World!

---
+++ ConsoleAppTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
